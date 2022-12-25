da1c44ae5e2bc1c20e54604603f55540c802848c
AngelicaGuimaraes <angelicaguimaraes76@gmail.com>
2022-12-25 18:32:37 +0000

    create class Category in models.py

--- magnolia_tree/models.py
+++ magnolia_tree/models.py
@@ -8,4 +8,32 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Create your models here.
+class Category(models.Model):
+    """
+    Model for categories for blogposts stating category of
+    Yoga.
+    """
+    category_name = models.CharField(
+        max_length=200,
+        blank=True,
+        unique=True,
+        help_text='Enter type of yoga'
+        )
+
+    class Meta:
+        """
+        Meta fields for ordering
+        """
+        ordering = ['category_name']
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return f'{self.category_name}'
+
+    def get_absolute_url(self):
+        """
+        Returns the URL to access a category
+        """
+        return reverse('category_name', args=[str(self.id)])

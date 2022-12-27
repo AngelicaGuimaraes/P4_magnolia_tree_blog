351480356c1662d278b33369f4e234f98e6550e3
AngelicaGuimaraes <angelicaguimaraes76@gmail.com>
2022-12-27 11:58:29 +0000

    add forms for comments and blog post in forms

---
+++ magnolia_tree/form.py
@@ -0,0 +1,46 @@
+"""
+Forms for magnolia tree blog page
+"""
+from django import forms
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+from .models import BlogComment, BlogPost
+
+
+class CommentForm(forms.ModelForm):
+    """
+    Form for adding comments
+    """
+    class Meta:
+        model = BlogComment
+        fields = ('body',)
+
+
+class CreateBlogPost(forms.ModelForm):
+    """
+    Form for creating a blog post
+    """
+    class Meta:
+        """
+        States what fields shall be seen in the form
+        """
+        model = BlogPost
+        fields = (
+            'title',
+            'category',
+            'section',
+            'content',
+            'excerpt',
+            'featured_image',
+        )
+        widgets = {
+            'content': SummernoteWidget(),
+            'excerpt': SummernoteWidget(),
+        }
+        labels = {
+            'title': 'Title of your blogpost',
+            'category': 'Chose category',
+            'section': 'Chose affected area',
+            'content': 'Your post',
+            'excerpt': 'Summary',
+            'featured_image': 'Upload image',
+        }

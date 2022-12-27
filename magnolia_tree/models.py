4074d3af5c4694309415a6b8c16cfdb7cf26efc4
AngelicaGuimaraes <angelicaguimaraes76@gmail.com>
2022-12-27 10:41:38 +0000

    add model section to models

--- magnolia_tree/models.py
+++ magnolia_tree/models.py
@@ -4,6 +4,7 @@ Models for Magnolia Tree Blog
 
 from django.db import models
 
+
 class Category(models.Model):
     """
     Model for categories for blogposts stating category of
@@ -34,4 +35,39 @@ class Category(models.Model):
         """
         return reverse('category_name', args=[str(self.id)])
 
-        
+
+class Section(models.Model):
+    """
+    Model for section of body affected by the yoga
+    """
+    section_name = models.CharField(
+        max_length=200,
+        blank=True,
+        unique=True,
+        help_text='Enter a section of the body'
+        )
+
+    class Meta:
+        """
+        Meta fields for ordering
+        """
+        ordering = ['section_name']
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return f'{self.section_name}'
+
+    def get_absolute_url(self):
+        """
+        Returns the URL to access a section
+        """
+        return reverse('section_name', args=[str(self.id)])
+
+
+
+
+
+
+       

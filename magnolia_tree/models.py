0615d39888211162dc3ed4215332422939bad4f9
AngelicaGuimaraes <angelicaguimaraes76@gmail.com>
2022-12-27 10:43:30 +0000

    add imports in models

--- magnolia_tree/models.py
+++ magnolia_tree/models.py
@@ -1,8 +1,11 @@
 """
-Models for Magnolia Tree Blog
+Models for Magnolia Tree
 """
-
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Category(models.Model):

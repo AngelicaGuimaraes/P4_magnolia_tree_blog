a20f564b585301d7b34ae4b2514bd8dc5363270d
AngelicaGuimaraes <angelicaguimaraes76@gmail.com>
2022-12-29 08:44:25 +0000

    fix allowed host on settings

--- blog/settings.py
+++ blog/settings.py
@@ -30,9 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["blog.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["magnolia-tree.herokuapp.com", "localhost"]
 
 
 # Application definition
